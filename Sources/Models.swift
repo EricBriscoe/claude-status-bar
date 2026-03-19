5b71c642a17e8484827b1c98f35f5ecd5e273138
Eric Briscoe <eric0briscoe@gmail.com>
2026-03-18 21:20:12 -0500

    Extract shared JSONDecoder.snakeCase extension
    DRY up duplicate decoder setup across MenuBarController and UpdateChecker.

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+extension JSONDecoder {
+    static let snakeCase: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+}
+
 struct StatusSummary: Codable {
     let status: OverallStatus
     let components: [ServiceComponent]
